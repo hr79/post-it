8257392260eed84fff2345df1dafcc56791a1707
hr79 <doox2oo4114@gmail.com>
2025-06-27 05:23:03 +0900

    feat: 로그인 요청 dto, 회원가입 요청 dto 추가

---
+++ postIt-backend/src/main/java/com/example/postItBackend/domain/auth/dto/LoginRequestDto.java
@@ -0,0 +1,16 @@
+package com.example.postItBackend.domain.auth.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class LoginRequestDto {
+    private String username;
+    private String password;
+
+    public LoginRequestDto(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+}
