2959ffafb24dbe21720285bb74200399caa84ec1
hr79 <doox2oo4114@gmail.com>
2025-07-03 23:39:02 +0900

    chore: Update app_route.dart

--- postIt-frontend/lib/app_route.dart
+++ postIt-frontend/lib/app_route.dart
@@ -1,5 +1,5 @@
 
 class AppRoute {
   // static const basedUrl = "http://localhost:8080/api";
-  static const basedUrl = "http://13.209.85.84/api";
+  static const basedUrl = "http://3.39.171.6/api";
 }
