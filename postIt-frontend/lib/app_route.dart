7677e4bb322c2caf7f55d054765e3c41dddb637b
hr79 <doox2oo4114@gmail.com>
2025-07-05 10:56:55 +0900

    fix: basedUrl static 누락 수정

--- postIt-frontend/lib/app_route.dart
+++ postIt-frontend/lib/app_route.dart
@@ -1,5 +1,5 @@
 class AppRoute {
-  final basedUrl = Uri.base.origin.contains("localhost")
+  static final basedUrl = Uri.base.origin.contains("localhost")
       ? "http://localhost:8080/api"
       : "https://post-it-service.shop/api";
 }
