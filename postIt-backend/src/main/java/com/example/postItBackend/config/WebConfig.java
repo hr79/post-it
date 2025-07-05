d5787e07ff30b4d5a4c019d88da5dfc43aabb28d
hr79 <doox2oo4114@gmail.com>
2025-07-05 10:36:12 +0900

    fix: WebConfig의 cors 세팅에 dns 추가

--- postIt-backend/src/main/java/com/example/postItBackend/config/WebConfig.java
+++ postIt-backend/src/main/java/com/example/postItBackend/config/WebConfig.java
@@ -11,6 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:8081")
                 .allowedOrigins("http://3.39.171.6/")
+                .allowedOrigins("https://post-it-service.shop/")
 //                .allowedOrigins("http://localhost")
 //                .allowedOrigins("http://localhost")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
