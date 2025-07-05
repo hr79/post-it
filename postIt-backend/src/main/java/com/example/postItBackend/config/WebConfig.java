e92b97358f0cc430b7bc72986def9e8a8b4e67aa
hr79 <doox2oo4114@gmail.com>
2025-07-05 11:22:16 +0900

    fix: WebConfig .allowedOrigins 수정

--- postIt-backend/src/main/java/com/example/postItBackend/config/WebConfig.java
+++ postIt-backend/src/main/java/com/example/postItBackend/config/WebConfig.java
@@ -9,11 +9,11 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8081")
-                .allowedOrigins("http://3.39.171.6")
-                .allowedOrigins("https://post-it-service.shop")
-//                .allowedOrigins("http://localhost")
-//                .allowedOrigins("http://localhost")
+                .allowedOrigins(
+                        "http://localhost:8081",
+                        "http://3.39.171.6",
+                        "https://post-it-service.shop"
+                )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
