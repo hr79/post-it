2ad1e8e9880f65268bbb2772d61dd16f2d982bb0
hr79 <doox2oo4114@gmail.com>
2025-07-06 22:47:50 +0900

    fix: 소셜로그인 콜백 후 브라우저에서 팝업을 인식하지 못해 넘어가지 못하는 문제 수정

--- postIt-frontend/lib/view/social_login_callback_page.dart
+++ postIt-frontend/lib/view/social_login_callback_page.dart
@@ -26,16 +26,23 @@ class SocialLoginCallbackPage extends GetView<SocialLoginCallbackController> {
         controller.saveToken(accessToken);
 
         // 팝업이면 닫고, 아니면 리디렉션
-        if (html.window.opener != null) {
+        final opener = html.window.opener;
+
+        if (opener != null) {
           print("팝업 상태입니다");
-          html.window.opener!
-              .postMessage({'accessToken': accessToken}, uri.origin);
+          try {
+            opener.postMessage({'accessToken': accessToken}, uri.origin);
+          } catch (e) {
+            print('postMessage 오류: $e');
+          }
           html.window.close(); // 팝업이면 자동으로 닫힘
         } else {
           // 새 탭으로 열린 경우: 홈으로 리디렉트
           print("새탭 상태입니다");
           Get.offAllNamed('/');
         }
+      } else {
+        print("액세스 토큰이 없습니다");
       }
     });
 
