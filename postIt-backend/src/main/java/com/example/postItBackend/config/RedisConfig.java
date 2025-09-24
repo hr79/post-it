de53adfea36ad519d85759525544b7227485a1b1
hr79 <doox2oo4114@gmail.com>
2025-09-25 06:41:13 +0900

    chore: RedisConfig redisPassword 제거

--- postIt-backend/src/main/java/com/example/postItBackend/config/RedisConfig.java
+++ postIt-backend/src/main/java/com/example/postItBackend/config/RedisConfig.java
@@ -18,8 +18,8 @@ public class RedisConfig {
     @Value("${spring.data.redis.port}")
     private int redisPort;
 
-    @Value("${spring.data.redis.password}")
-    private String redisPassword;
+//    @Value("${spring.data.redis.password}")
+//    private String redisPassword;
 
     @Bean
     public LettuceConnectionFactory lettuceConnectionFactory() {
