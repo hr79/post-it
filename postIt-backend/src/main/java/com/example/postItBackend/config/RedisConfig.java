5c82f43cc1a8e12d327ef8e3caedcadd5aeadc45
hr79 <doox2oo4114@gmail.com>
2025-09-25 23:00:25 +0900

    fix: unable to connect to redis
    * chore: lettuce 버전 업데이트(fail)
    
    * chore: redisconfig resp2 protocol 적용

--- postIt-backend/src/main/java/com/example/postItBackend/config/RedisConfig.java
+++ postIt-backend/src/main/java/com/example/postItBackend/config/RedisConfig.java
@@ -1,10 +1,13 @@
 package com.example.postItBackend.config;
 
+import io.lettuce.core.ClientOptions;
+import io.lettuce.core.protocol.ProtocolVersion;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
@@ -23,7 +26,14 @@ public class RedisConfig {
 
     @Bean
     public LettuceConnectionFactory lettuceConnectionFactory() {
-        return new LettuceConnectionFactory(new RedisStandaloneConfiguration(redisHost, redisPort));
+        // RESP2 프로토콜 강제 설정
+        LettuceClientConfiguration clientConfig = LettuceClientConfiguration.builder()
+                .clientOptions(ClientOptions.builder()
+                        .protocolVersion(ProtocolVersion.RESP2)
+                        .build())
+                .build();
+
+        return new LettuceConnectionFactory(new RedisStandaloneConfiguration(redisHost, redisPort),  clientConfig);
     }
 
     @Bean
