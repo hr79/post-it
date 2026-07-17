ddb861c9f6a8e4c8f6d2df896e846bc3a1abaec0
hr79 <doox2oo4114@gmail.com>
2026-07-17 19:15:20 +0900

    chore: 도메인 주소 변경

--- postIt-frontend/lib/app_route.dart
+++ postIt-frontend/lib/app_route.dart
@@ -1,5 +1,5 @@
 class AppRoute {
   static final basedUrl = Uri.base.origin.contains("localhost")
       ? "http://localhost:8080/api"
-      : "https://post-it-service.shop/api";
+      : "https://post-it-00-service.duckdns.org/api";
 }
